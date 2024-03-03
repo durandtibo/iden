cc99cd99989f0bc3884333e02882c5da5cd7bbb1
Thibaut Durand <durand.tibo@gmail.com>
2024-03-03 15:31:06 -0800

    Add abstract factory to `BaseShardLoader` (#13)

--- src/iden/shard/loader/__init__.py
+++ src/iden/shard/loader/__init__.py
@@ -2,7 +2,11 @@ r"""Contain shard loader implementations."""
 
 from __future__ import annotations
 
-__all__ = ["BaseShardLoader", "PickleShardLoader"]
+__all__ = ["BaseShardLoader", "PickleShardLoader", "is_shard_loader_config", "setup_shard_loader"]
 
-from iden.shard.loader.base import BaseShardLoader
+from iden.shard.loader.base import (
+    BaseShardLoader,
+    is_shard_loader_config,
+    setup_shard_loader,
+)
 from iden.shard.loader.pickle_ import PickleShardLoader
---
+++ tests/unit/shard/loader/test_base.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from objectory import OBJECT_TARGET
+
+from iden.shard import PickleShard
+from iden.shard.loader import (
+    PickleShardLoader,
+    is_shard_loader_config,
+    setup_shard_loader,
+)
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    import pytest
+
+############################################
+#     Tests for is_shard_loader_config     #
+############################################
+
+
+def test_is_shard_loader_config_true() -> None:
+    assert is_shard_loader_config({OBJECT_TARGET: "iden.shard.loader.PickleShardLoader"})
+
+
+def test_is_shard_loader_config_false() -> None:
+    assert not is_shard_loader_config({OBJECT_TARGET: "iden.shard.PickleShard"})
+
+
+########################################
+#     Tests for setup_shard_loader     #
+########################################
+
+
+def test_setup_shard_loader_object() -> None:
+    generator = PickleShardLoader()
+    assert setup_shard_loader(generator) is generator
+
+
+def test_setup_shard_loader_dict() -> None:
+    assert isinstance(
+        setup_shard_loader({OBJECT_TARGET: "iden.shard.loader.PickleShardLoader"}),
+        PickleShardLoader,
+    )
+
+
+def test_setup_shard_loader_incorrect_type(
+    caplog: pytest.LogCaptureFixture, tmp_path: Path
+) -> None:
+    with caplog.at_level(level=logging.WARNING):
+        assert isinstance(
+            setup_shard_loader(
+                {OBJECT_TARGET: "iden.shard.PickleShard", "uri": "", "path": tmp_path}
+            ),
+            PickleShard,
+        )
+        assert caplog.messages
