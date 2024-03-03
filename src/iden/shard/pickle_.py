bc49133a04c25c497e82c6dbdd7e11cccb98642c
Thibaut Durand <durand.tibo@gmail.com>
2024-03-02 22:34:03 -0800

    Update `PickleShard` (#11)

--- src/iden/shard/pickle_.py
+++ src/iden/shard/pickle_.py
@@ -2,12 +2,12 @@ r"""Contain in-memory shard implementations."""
 
 from __future__ import annotations
 
-__all__ = ["PickleShard"]
+__all__ = ["PickleShard", "save_uri_file"]
 
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar
 
 from iden.shard.base import BaseShard
-from iden.utils.io import load_pickle
+from iden.utils.io import load_json, load_pickle, save_json
 from iden.utils.path import sanitize_path
 
 if TYPE_CHECKING:
@@ -22,7 +22,8 @@ class PickleShard(BaseShard[T]):
     The data are stored in a pickle file.
 
     Args:
-        path: Specifies the path where to store the pickle file.
+        uri: The URI associated to the shard.
+        path: Specifies the path to the pickle file.
 
     Example usage:
 
@@ -34,7 +35,7 @@ class PickleShard(BaseShard[T]):
     >>> with tempfile.TemporaryDirectory() as tmpdir:
     ...     path = Path(tmpdir).joinpath("data.pkl")
     ...     save_pickle([1, 2, 3], path)
-    ...     shard = PickleShard(path)
+    ...     shard = PickleShard(uri="file:///data/1234456789", path=path)
     ...     shard.get_data()
     ...
     [1, 2, 3]
@@ -42,12 +43,23 @@ class PickleShard(BaseShard[T]):
     ```
     """
 
-    def __init__(self, path: Path | str) -> None:
+    def __init__(self, uri: str, path: Path | str) -> None:
+        self._uri = uri
         self._path = sanitize_path(path)
         self._data = None
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}(uri={self.get_uri()})"
+
+    @property
+    def path(self) -> Path:
+        r"""The path to the pickle file."""
+        return self._path
+
+    def equal(self, other: Any) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.get_uri() == other.get_uri() and self.path == other.path
 
     def get_data(self) -> T:
         if self._data is None:
@@ -55,4 +67,32 @@ class PickleShard(BaseShard[T]):
         return self._data
 
     def get_uri(self) -> str:
-        return self._path.as_uri()
+        return self._uri
+
+    @classmethod
+    def from_uri(cls, uri: str) -> PickleShard:
+        config = load_json(sanitize_path(uri))
+        return cls(uri=uri, path=config["path"])
+
+
+def save_uri_file(uri: str, path: Path | str) -> None:
+    r"""Save the Uniform Resource Identifier (URI) file.
+
+    Args:
+        uri: The URI associated to the shard.
+        path: Specifies the path to the pickle file.
+
+    Example usage:
+
+    ```pycon
+    >>> import tempfile
+    >>> from pathlib import Path
+    >>> from iden.shard.pickle_ import save_uri_file
+    >>> with tempfile.TemporaryDirectory() as tmpdir:
+    ...     path = Path(tmpdir).joinpath("data.pkl")
+    ...     save_uri_file(uri="file:///data/1234456789", path=path)  # xdoctest: +SKIP()
+    ...
+
+    ```
+    """
+    save_json({"path": sanitize_path(path).as_posix()}, sanitize_path(uri))
--- tests/unit/shard/test_pickle.py
+++ tests/unit/shard/test_pickle.py
@@ -5,38 +5,86 @@ from typing import TYPE_CHECKING
 import pytest
 
 from iden.shard import PickleShard
-from iden.utils.io import save_pickle
+from iden.shard.pickle_ import save_uri_file
+from iden.utils.io import load_json, save_pickle
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+
+@pytest.fixture(scope="module")
+def path(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    path_ = tmp_path_factory.mktemp("tmp").joinpath("data.pkl")
+    save_pickle([1, 2, 3], path_)
+    return path_
+
+
+@pytest.fixture(scope="module")
+def uri(tmp_path_factory: pytest.TempPathFactory, path: Path) -> str:
+    uri_ = tmp_path_factory.mktemp("tmp").joinpath("uri").as_uri()
+    save_uri_file(uri=uri_, path=path)
+    return uri_
+
 
 #################################
 #     Tests for PickleShard     #
 #################################
 
 
-@pytest.fixture(scope="module")
-def path(tmp_path_factory: pytest.TempPathFactory) -> Path:
-    p = tmp_path_factory.mktemp("tmp").joinpath("data.pkl")
-    save_pickle([1, 2, 3], p)
-    return p
+def test_pickle_shard_str(uri: str, path: Path) -> None:
+    assert str(PickleShard(uri=uri, path=path)).startswith("PickleShard(")
 
 
-def test_pickle_shard_str(path: Path) -> None:
-    assert str(PickleShard(path)).startswith("PickleShard(")
+def test_pickle_shard_path(uri: str, path: Path) -> None:
+    assert PickleShard(uri=uri, path=path).path == path
 
 
-def test_pickle_shard_get_data(path: Path) -> None:
-    assert PickleShard(path).get_data() == [1, 2, 3]
+def test_pickle_shard_equal_true(uri: str, path: Path) -> None:
+    assert PickleShard(uri=uri, path=path).equal(PickleShard(uri=uri, path=path))
 
 
-def test_pickle_shard_get_data_multiple_calls(path: Path) -> None:
-    shard = PickleShard(path)
+def test_pickle_shard_equal_false_different_uri(uri: str, path: Path) -> None:
+    assert not PickleShard(uri=uri, path=path).equal(PickleShard(uri="", path=path))
+
+
+def test_pickle_shard_equal_false_different_path(uri: str, path: Path, tmp_path: Path) -> None:
+    assert not PickleShard(uri=uri, path=path).equal(PickleShard(uri=uri, path=tmp_path))
+
+
+def test_pickle_shard_equal_false_different_type(uri: str, path: Path) -> None:
+    assert not PickleShard(uri=uri, path=path).equal(42)
+
+
+def test_pickle_shard_get_data(uri: str, path: Path) -> None:
+    assert PickleShard(uri=uri, path=path).get_data() == [1, 2, 3]
+
+
+def test_pickle_shard_get_data_multiple_calls(uri: str, path: Path) -> None:
+    shard = PickleShard(uri=uri, path=path)
     assert shard.get_data() == [1, 2, 3]
     assert shard.get_data() == [1, 2, 3]
     shard.get_data().append(4)
     assert shard.get_data() == [1, 2, 3, 4]
 
 
-def test_pickle_shard_get_uri(path: Path) -> None:
-    assert PickleShard(path).get_uri() == path.as_uri()
+def test_pickle_shard_get_uri(uri: str, path: Path) -> None:
+    assert PickleShard(uri=uri, path=path).get_uri() == uri
+
+
+def test_pickle_shard_from_uri(uri: str, path: Path) -> None:
+    shard = PickleShard.from_uri(uri)
+    assert shard.equal(PickleShard(uri=uri, path=path))
+    assert shard.get_data() == [1, 2, 3]
+
+
+###################################
+#     Tests for save_uri_file     #
+###################################
+
+
+def test_save_uri_file(tmp_path: Path) -> None:
+    uri = tmp_path.joinpath("uri")
+    path = tmp_path.joinpath("data.pkl")
+    save_uri_file(uri=uri.as_uri(), path=path)
+    assert uri.is_file()
+    assert load_json(uri) == {"path": path.as_posix()}
