2d05ec09a0c62ae10df5227d9fe936386b1fa2db
Thibaut Durand <durand.tibo@gmail.com>
2024-03-03 22:32:08 -0800

    Add `NumpySafetensorsSaver`

--- src/iden/io/safetensors/__init__.py
+++ src/iden/io/safetensors/__init__.py
@@ -2,6 +2,6 @@ r"""Contain safetensors data loaders and savers."""
 
 from __future__ import annotations
 
-__all__ = ["TorchSafetensorsSaver"]
+__all__ = ["NumpySafetensorsSaver", "TorchSafetensorsSaver"]
 
-from iden.io.safetensors.savers import TorchSafetensorsSaver
+from iden.io.safetensors.savers import NumpySafetensorsSaver, TorchSafetensorsSaver
