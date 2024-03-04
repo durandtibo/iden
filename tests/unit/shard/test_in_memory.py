116ac44ce58826cde22b1a22d0d21ef55b4563a0
Thibaut Durand <durand.tibo@gmail.com>
2024-03-03 20:17:12 -0800

    Add `equal` to `BaseShard`

--- tests/unit/shard/test_in_memory.py
+++ tests/unit/shard/test_in_memory.py
@@ -8,12 +8,24 @@ from iden.shard import InMemoryShard
 
 
 def test_in_memory_shard_str() -> None:
-    assert str(InMemoryShard(1)) == "InMemoryShard()"
+    assert str(InMemoryShard([1, 2, 3])) == "InMemoryShard()"
+
+
+def test_in_memory_shard_equal_true() -> None:
+    assert InMemoryShard([1, 2, 3]).equal(InMemoryShard([1, 2, 3]))
+
+
+def test_in_memory_shard_equal_false_different_data() -> None:
+    assert not InMemoryShard([1, 2, 3]).equal(InMemoryShard([1, 2, 4]))
+
+
+def test_in_memory_shard_equal_false_different_type() -> None:
+    assert not InMemoryShard([1, 2, 3]).equal([1, 2, 4])
 
 
 def test_in_memory_shard_get_data() -> None:
-    assert InMemoryShard(1).get_data() == 1
+    assert InMemoryShard([1, 2, 3]).get_data() == [1, 2, 3]
 
 
 def test_in_memory_shard_get_uri() -> None:
-    assert InMemoryShard(1).get_uri() is None
+    assert InMemoryShard([1, 2, 3]).get_uri() is None
