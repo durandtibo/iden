7e43221ec84b27b62b8ebb2aeb8520960d6b4b5d
Thibaut Durand <durand.tibo@gmail.com>
2024-03-03 22:55:24 -0800

    Add `NumpySafetensorsSaver` (#19)

--- src/iden/io/safetensors/savers.py
+++ src/iden/io/safetensors/savers.py
@@ -2,12 +2,12 @@ r"""Contain implementation of savers that use the safetensors format."""
 
 from __future__ import annotations
 
-__all__ = ["TorchSafetensorsSaver"]
+__all__ = ["NumpySafetensorsSaver", "TorchSafetensorsSaver"]
 
 from typing import TYPE_CHECKING
 from unittest.mock import Mock
 
-from coola.utils import check_torch, is_torch_available
+from coola.utils import check_numpy, check_torch, is_numpy_available, is_torch_available
 
 from iden.io import BaseFileSaver
 from iden.utils.imports import check_safetensors, is_safetensors_available
@@ -15,15 +15,42 @@ from iden.utils.imports import check_safetensors, is_safetensors_available
 if TYPE_CHECKING:
     from pathlib import Path
 
+if is_numpy_available():
+    import numpy as np
+else:
+    np = Mock()  # pragma: no cover
+
 if is_safetensors_available():
+    from safetensors import numpy as sn
     from safetensors import torch as st
 else:  # pragma: no cover
+    sn = Mock()
     st = Mock()
 
 if is_torch_available():
     import torch
 else:  # pragma: no cover
     torch = Mock()
+
+
+class NumpySafetensorsSaver(BaseFileSaver[dict[str, np.ndarray]]):
+    r"""Implement a file saver to save ``numpy.ndarray``s with the
+    safetensors format.
+
+    This saver can only save a dictionary of ``numpy.ndarray``s.
+
+    Link: https://huggingface.co/docs/safetensors/en/index
+    """
+
+    def __init__(self) -> None:
+        check_safetensors()
+        check_numpy()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
+
+    def _save_file(self, to_save: dict[str, np.ndarray], path: Path) -> None:
+        sn.save_file(to_save, path)
 
 
 class TorchSafetensorsSaver(BaseFileSaver[dict[str, torch.Tensor]]):
