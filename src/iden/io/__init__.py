a135791826ca532eca376df1305ff26d02040399
Thibaut Durand <durand.tibo@gmail.com>
2024-03-04 18:16:16 -0800

    Add `JsonLoader` and `JsonSaver`

--- src/iden/io/__init__.py
+++ src/iden/io/__init__.py
@@ -6,16 +6,21 @@ __all__ = [
     "BaseFileSaver",
     "BaseLoader",
     "BaseSaver",
+    "JsonLoader",
+    "JsonSaver",
     "PickleLoader",
     "PickleSaver",
     "TextLoader",
     "TextSaver",
+    "load_json",
     "load_pickle",
     "load_text",
+    "save_json",
     "save_pickle",
     "save_text",
 ]
 
 from iden.io.base import BaseFileSaver, BaseLoader, BaseSaver
+from iden.io.json import JsonLoader, JsonSaver, load_json, save_json
 from iden.io.pickle import PickleLoader, PickleSaver, load_pickle, save_pickle
 from iden.io.text import TextLoader, TextSaver, load_text, save_text
