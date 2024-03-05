1d9dccd256437e553f450a5007a55f006826657c
Thibaut Durand <durand.tibo@gmail.com>
2024-03-04 17:39:52 -0800

    Improve text loader/saver (#24)

--- tests/unit/io/test_text.py
+++ tests/unit/io/test_text.py
@@ -4,8 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from iden.io import TextLoader, TextSaver
-from iden.utils.io import load_text, save_text
+from iden.io import TextLoader, TextSaver, load_text, save_text
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -14,7 +13,7 @@ if TYPE_CHECKING:
 @pytest.fixture(scope="module")
 def path_text(tmp_path_factory: pytest.TempPathFactory) -> Path:
     path = tmp_path_factory.mktemp("tmp").joinpath("data.txt")
-    TextSaver().save("hello", path)
+    save_text("hello", path)
     return path
 
 
@@ -47,6 +46,14 @@ def test_text_saver_save(tmp_path: Path) -> None:
     assert path.is_file()
 
 
+def test_text_saver_save_list(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    saver = TextSaver()
+    saver.save([1, 2, 3], path)
+    assert path.is_file()
+    assert load_text(path) == "[1, 2, 3]"
+
+
 def test_text_saver_save_file_exist(tmp_path: Path) -> None:
     path = tmp_path.joinpath("tmp/data.txt")
     save_text("hello", path)
@@ -70,3 +77,52 @@ def test_text_saver_save_file_exist_ok_dir(tmp_path: Path) -> None:
     saver = TextSaver()
     with pytest.raises(IsADirectoryError, match="path .* is a directory"):
         saver.save("hello", path)
+
+
+###############################
+#     Tests for load_text     #
+###############################
+
+
+def test_load_text_load(path_text: Path) -> None:
+    assert load_text(path_text) == "hello"
+
+
+###############################
+#     Tests for save_text     #
+###############################
+
+
+def test_save_text(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    save_text("hello", path)
+    assert path.is_file()
+
+
+def test_save_text_list(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    save_text([1, 2, 3], path)
+    assert path.is_file()
+    assert load_text(path) == "[1, 2, 3]"
+
+
+def test_save_text_file_exist(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    save_text("hello", path)
+    with pytest.raises(FileExistsError, match="path .* already exists."):
+        save_text("hello", path)
+
+
+def test_save_text_file_exist_ok(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    save_text("hello", path)
+    save_text("meow", path, exist_ok=True)
+    assert path.is_file()
+    assert load_text(path) == "meow"
+
+
+def test_save_text_file_exist_ok_dir(tmp_path: Path) -> None:
+    path = tmp_path.joinpath("tmp/data.txt")
+    path.mkdir(parents=True, exist_ok=True)
+    with pytest.raises(IsADirectoryError, match="path .* is a directory"):
+        save_text("hello", path)
