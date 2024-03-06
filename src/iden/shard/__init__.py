697451f9f191655dd854d9ef12a9f6d63f830c1b
Thibaut Durand <durand.tibo@gmail.com>
2024-03-05 23:11:13 -0800

    Add `FileShard` (#36)

--- src/iden/shard/__init__.py
+++ src/iden/shard/__init__.py
@@ -2,9 +2,10 @@ r"""Contain shard implementations."""
 
 from __future__ import annotations
 
-__all__ = ["BaseShard", "InMemoryShard", "PickleShard", "load_from_uri"]
+__all__ = ["BaseShard", "FileShard", "InMemoryShard", "PickleShard", "load_from_uri"]
 
 from iden.shard.base import BaseShard
+from iden.shard.file import FileShard
 from iden.shard.in_memory import InMemoryShard
 from iden.shard.loading import load_from_uri
 from iden.shard.pickle import PickleShard
