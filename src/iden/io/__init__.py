20e3d8abf6620bd2170715e5c59f0e5b4ee76bbf
Thibaut Durand <durand.tibo@gmail.com>
2024-03-05 22:58:14 -0800

    Add `AutoFileLoader` (#35)

--- src/iden/io/__init__.py
+++ src/iden/io/__init__.py
@@ -3,6 +3,7 @@ r"""Contain data loaders and savers."""
 from __future__ import annotations
 
 __all__ = [
+    "AutoFileLoader",
     "BaseFileSaver",
     "BaseLoader",
     "BaseSaver",
@@ -32,6 +33,7 @@ __all__ = [
     "setup_saver",
 ]
 
+from iden.io.auto import AutoFileLoader, register_auto_loaders
 from iden.io.base import (
     BaseFileSaver,
     BaseLoader,
@@ -46,3 +48,5 @@ from iden.io.pickle import PickleLoader, PickleSaver, load_pickle, save_pickle
 from iden.io.text import TextLoader, TextSaver, load_text, save_text
 from iden.io.torch import TorchLoader, TorchSaver, load_torch, save_torch
 from iden.io.yaml import YamlLoader, YamlSaver, load_yaml, save_yaml
+
+register_auto_loaders()
