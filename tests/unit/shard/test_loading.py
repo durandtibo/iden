ba00f9a7779584d9c6851516fe03024fc440730c
Thibaut Durand <durand.tibo@gmail.com>
2024-03-07 20:45:15 -0800

    Improve testing of `load_from_uri` (#49)

--- tests/unit/shard/test_loading.py
+++ tests/unit/shard/test_loading.py
@@ -9,10 +9,16 @@ from coola.testing import torch_available
 from coola.utils import is_torch_available
 
 from iden.shard import (
+    JsonShard,
     PickleShard,
     TorchSafetensorsShard,
+    TorchShard,
+    YamlShard,
+    create_json_shard,
     create_pickle_shard,
     create_torch_safetensors_shard,
+    create_torch_shard,
+    create_yaml_shard,
     load_from_uri,
 )
 from iden.testing import safetensors_available
@@ -31,13 +37,22 @@ else:  # pragma: no cover
 ###################################
 
 
+def test_load_from_uri_json(tmp_path: Path) -> None:
+    uri = tmp_path.joinpath("my_uri").as_uri()
+    path = tmp_path.joinpath("my_uri.json")
+    create_json_shard(data={"key1": [1, 2, 3], "key2": "abc"}, uri=uri, path=path)
+    shard = load_from_uri(uri)
+    assert shard.equal(JsonShard(uri=uri, path=path))
+    assert objects_are_equal(shard.get_data(), {"key1": [1, 2, 3], "key2": "abc"})
+
+
 def test_load_from_uri_pickle(tmp_path: Path) -> None:
     uri = tmp_path.joinpath("my_uri").as_uri()
     path = tmp_path.joinpath("my_uri.pkl")
-    create_pickle_shard(data=[1, 2, 3], uri=uri, path=path)
+    create_pickle_shard(data={"key1": [1, 2, 3], "key2": "abc"}, uri=uri, path=path)
     shard = load_from_uri(uri)
     assert shard.equal(PickleShard(uri=uri, path=path))
-    assert objects_are_equal(shard.get_data(), [1, 2, 3])
+    assert objects_are_equal(shard.get_data(), {"key1": [1, 2, 3], "key2": "abc"})
 
 
 @safetensors_available
@@ -51,6 +66,25 @@ def test_load_from_uri_torch_safetensors(tmp_path: Path) -> None:
     shard = load_from_uri(uri)
     assert shard.equal(TorchSafetensorsShard(uri=uri, path=path))
     assert objects_are_equal(shard.get_data(), {"key1": torch.ones(2, 3), "key2": torch.arange(5)})
+
+
+@torch_available
+def test_load_from_uri_torch(tmp_path: Path) -> None:
+    uri = tmp_path.joinpath("my_uri").as_uri()
+    path = tmp_path.joinpath("my_uri.pt")
+    create_torch_shard(data={"key1": torch.ones(2, 3), "key2": torch.arange(5)}, uri=uri, path=path)
+    shard = load_from_uri(uri)
+    assert shard.equal(TorchShard(uri=uri, path=path))
+    assert objects_are_equal(shard.get_data(), {"key1": torch.ones(2, 3), "key2": torch.arange(5)})
+
+
+def test_load_from_uri_yaml(tmp_path: Path) -> None:
+    uri = tmp_path.joinpath("my_uri").as_uri()
+    path = tmp_path.joinpath("my_uri.yaml")
+    create_yaml_shard(data={"key1": [1, 2, 3], "key2": "abc"}, uri=uri, path=path)
+    shard = load_from_uri(uri)
+    assert shard.equal(YamlShard(uri=uri, path=path))
+    assert objects_are_equal(shard.get_data(), {"key1": [1, 2, 3], "key2": "abc"})
 
 
 def test_load_from_uri_missing() -> None:
