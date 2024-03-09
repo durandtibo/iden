7a0414effdbc2600adb18d4fbaf1f559e11eb4a4
Thibaut Durand <durand.tibo@gmail.com>
2024-03-09 13:40:53 -0800

    Add `ShardMapping` (#59)

---
+++ src/iden/shard/collection/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain data structures to manage a collection of shards."""
+
+from __future__ import annotations
+
+__all__ = ["ShardMapping"]
+
+from iden.shard.collection.mapping import ShardMapping
