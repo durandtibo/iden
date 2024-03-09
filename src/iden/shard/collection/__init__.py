fdf5ffeb34429cade7bbd9d978a03eba184add64
Thibaut Durand <durand.tibo@gmail.com>
2024-03-09 14:19:12 -0800

    Add `ShardList`

--- src/iden/shard/collection/__init__.py
+++ src/iden/shard/collection/__init__.py
@@ -2,6 +2,7 @@ r"""Contain data structures to manage a collection of shards."""
 
 from __future__ import annotations
 
-__all__ = ["ShardMapping"]
+__all__ = ["ShardMapping", "ShardList"]
 
+from iden.shard.collection.list import ShardList
 from iden.shard.collection.mapping import ShardMapping
