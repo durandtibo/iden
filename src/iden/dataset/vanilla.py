8a74a4934f03a84df09cfde232f4b5c23d05b4e3
Thibaut Durand <durand.tibo@gmail.com>
2024-03-08 22:32:17 -0800

    Improve `VanillaDataset` (#56)

--- src/iden/dataset/vanilla.py
+++ src/iden/dataset/vanilla.py
@@ -7,11 +7,16 @@ __all__ = ["VanillaDataset", "prepare_shards"]
 import logging
 from typing import TYPE_CHECKING, Any, TypeVar
 
+from coola import objects_are_equal
 from coola.utils import str_indent, str_mapping
+from objectory import OBJECT_TARGET
 
+from iden.constants import LOADER, SHARDS
 from iden.dataset.base import BaseDataset
 from iden.dataset.exceptions import AssetNotFoundError, SplitNotFoundError
+from iden.io import JsonSaver
 from iden.shard import sort_by_uri
+from iden.utils.path import sanitize_path
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
@@ -27,7 +32,8 @@ class VanillaDataset(BaseDataset[T]):
     r"""Implement a simple dataset.
 
     Args:
-        shards: The shards to prepare. Each item in the mapping
+        uri: The URI associated to the dataset.
+        shards: The dataset's shards. Each item in the mapping
             represent a dataset split, where the key is the dataset
             split and the value is the shards.
         assets: The dataset's assets.
@@ -42,15 +48,20 @@ class VanillaDataset(BaseDataset[T]):
     >>> with tempfile.TemporaryDirectory() as tmpdir:
     ...     shards = {
     ...         "train": [
-    ...             create_json_shard([1, 2, 3], uri=Path(tmpdir).joinpath("uri1").as_uri()),
-    ...             create_json_shard([4, 5, 6, 7], uri=Path(tmpdir).joinpath("uri2").as_uri()),
+    ...             create_json_shard(
+    ...                 [1, 2, 3], uri=Path(tmpdir).joinpath("shard/uri1").as_uri()
+    ...             ),
+    ...             create_json_shard(
+    ...                 [4, 5, 6, 7], uri=Path(tmpdir).joinpath("shard/uri2").as_uri()
+    ...             ),
     ...         ],
     ...         "val": [],
     ...     }
-    ...     dataset = VanillaDataset(shards)
+    ...     dataset = VanillaDataset(uri=Path(tmpdir).joinpath("uri").as_uri(), shards=shards)
     ...     print(dataset)
     ...
     VanillaDataset(
+      (uri): file:///.../uri
       (shards):
         (train): 2
         (val): 0
@@ -61,8 +72,9 @@ class VanillaDataset(BaseDataset[T]):
     """
 
     def __init__(
-        self, shards: Mapping[str, Iterable[BaseShard[T]]], assets: dict[str, Any] = None
+        self, uri: str, shards: Mapping[str, Iterable[BaseShard[T]]], assets: dict[str, Any] = None
     ) -> None:
+        self._uri = str(uri)
         self._shards = prepare_shards(shards)
         self._assets = assets or {}
 
@@ -75,12 +87,22 @@ class VanillaDataset(BaseDataset[T]):
         args = str_indent(
             str_mapping(
                 {
+                    "uri": self._uri,
                     "shards": shards,
                     "assets": sorted(self._assets.keys()),
                 }
             )
         )
         return f"{self.__class__.__qualname__}(\n  {args}\n)"
+
+    def equal(self, other: Any, equal_nan: bool = False) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return (
+            objects_are_equal(self.get_uri(), other.get_uri(), equal_nan=equal_nan)
+            and objects_are_equal(self._shards, other._shards, equal_nan=equal_nan)
+            and objects_are_equal(self._assets, other._assets, equal_nan=equal_nan)
+        )
 
     def get_asset(self, asset_id: str) -> Any:
         if asset_id not in self._assets:
@@ -108,6 +130,99 @@ class VanillaDataset(BaseDataset[T]):
 
     def has_split(self, split: str) -> bool:
         return split in self._shards
+
+    def get_uri(self) -> str:
+        return self._uri
+
+    @classmethod
+    def generate_uri_config(cls, shards: Mapping[str, Iterable[BaseShard[T]]]) -> dict:
+        r"""Generate the minimal config that is used to load the dataset
+        from its URI.
+
+        The config must be compatible with the JSON format.
+
+        Args:
+            shards: The shards in the dataset. Each item in the mapping
+                represent a dataset split, where the key is the dataset
+                split and the value is the shards.
+
+        Returns:
+            The minimal config to load the shard from its URI.
+
+        Example usage:
+
+        ```pycon
+        >>> import tempfile
+        >>> from pathlib import Path
+        >>> from iden.dataset import VanillaDataset
+        >>> from iden.shard import create_json_shard
+        >>> with tempfile.TemporaryDirectory() as tmpdir:
+        ...     shards = {
+        ...         "train": [
+        ...             create_json_shard(
+        ...                 [1, 2, 3], uri=Path(tmpdir).joinpath("shard/uri1").as_uri()
+        ...             ),
+        ...             create_json_shard(
+        ...                 [4, 5, 6, 7], uri=Path(tmpdir).joinpath("shard/uri2").as_uri()
+        ...             ),
+        ...         ],
+        ...         "val": [],
+        ...     }
+        ...     config = VanillaDataset.generate_uri_config(shards)
+        ...     print(config)
+        ...
+        {'loader': {'_target_': 'iden.dataset.loader.VanillaDatasetLoader'},
+         'shards': {'train': ('file:///.../uri1', 'file:///.../uri2'), 'val': ()}}
+
+        ```
+        """
+        return {
+            LOADER: {OBJECT_TARGET: "iden.dataset.loader.VanillaDatasetLoader"},
+            SHARDS: {
+                split: tuple(shard.get_uri() for shard in data) for split, data in shards.items()
+            },
+        }
+
+
+def create_vanilla_dataset(
+    shards: Mapping[str, Iterable[BaseShard[T]]],
+    uri: str,
+    assets: dict[str, Any] = None,
+) -> VanillaDataset:
+    r"""Create a ``VanillaDataset`` from its shards.
+
+    Note:
+        It is a utility function to create a ``VanillaDataset`` from
+            its shards and URI. It is possible to create a
+            ``VanillaDataset`` in other ways.
+
+    Args:
+        shards: The dataset's shards. Each item in the mapping
+            represent a dataset split, where the key is the dataset
+            split and the value is the shards.
+        uri: The URI associated to the dataset.
+        assets: The dataset's assets.
+
+    Returns:
+        The ``JsonShard`` object.
+
+    Example usage:
+
+    ```pycon
+    >>> import tempfile
+    >>> from pathlib import Path
+    >>> from iden.shard import create_json_shard
+    >>> with tempfile.TemporaryDirectory() as tmpdir:
+    ...     shard = create_json_shard([1, 2, 3], uri=Path(tmpdir).joinpath("my_uri").as_uri())
+    ...     shard.get_data()
+    ...
+    [1, 2, 3]
+
+    ```
+    """
+    logger.info(f"Saving URI file {uri}")
+    JsonSaver().save(VanillaDataset.generate_uri_config(shards), sanitize_path(uri))
+    return VanillaDataset(uri=uri, shards=shards, assets=assets)
 
 
 def prepare_shards(
