c503a21b17261495390fa0479491ec9cecbb15b4
Thibaut Durand <durand.tibo@gmail.com>
2024-03-10 00:59:05 -0800

    Remove `asset` package

--- src/iden/asset/__init__.py
+++
@@ -1,8 +0,0 @@
-r"""Contain asset implementations."""
-
-from __future__ import annotations
-
-__all__ = ["BaseAsset", "InMemoryAsset"]
-
-from iden.asset.base import BaseAsset
-from iden.asset.in_memory import InMemoryAsset
