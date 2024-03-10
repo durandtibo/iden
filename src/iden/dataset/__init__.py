f7d024c16cc3fa4b50a4c04dcc3e85c8face15aa
Thibaut Durand <durand.tibo@gmail.com>
2024-03-10 11:44:02 -0700

    Add `load_from_uri` for dataset

--- src/iden/dataset/__init__.py
+++ src/iden/dataset/__init__.py
@@ -2,7 +2,8 @@ r"""Contain dataset implementations."""
 
 from __future__ import annotations
 
-__all__ = ["BaseDataset", "VanillaDataset", "create_vanilla_dataset"]
+__all__ = ["BaseDataset", "VanillaDataset", "create_vanilla_dataset", "load_from_uri"]
 
 from iden.dataset.base import BaseDataset
+from iden.dataset.loading import load_from_uri
 from iden.dataset.vanilla import VanillaDataset, create_vanilla_dataset
