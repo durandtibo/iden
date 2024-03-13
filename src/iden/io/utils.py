68f3e761ff12a892cdfdcfab03387183714ba3ab
Thibaut Durand <durand.tibo@gmail.com>
2024-03-12 22:32:44 -0700

    Add function to return unique temporary path

--- src/iden/io/utils.py
+++ src/iden/io/utils.py
@@ -2,12 +2,50 @@ r"""Contain I/O utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["get_loader_mapping"]
+__all__ = ["generate_unique_tmp_path", "get_loader_mapping"]
 
+import uuid
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from iden.io import BaseLoader
+
+
+def generate_unique_tmp_path(path: Path) -> Path:
+    r"""Return a unique temporary path given a path.
+
+    This function updates the name to add a UUID.
+
+    Args:
+        path: The input path.
+
+    Returns:
+        The unique name.
+
+    Example usage:
+
+    ```pycon
+    >>> import tempfile
+    >>> from pathlib import Path
+    >>> from iden.io.utils import generate_unique_tmp_path
+    >>> with tempfile.TemporaryDirectory() as tmpdir:
+    ...     path = generate_unique_tmp_path(Path(tmpdir).joinpath("data.pt"))
+    ...     path
+    ...
+    PosixPath('/.../data-....pt')
+
+    ```
+    """
+    h = uuid.uuid4().hex
+    extension = "".join(path.suffixes)[1:]
+    if extension:
+        extension = "." + extension
+        stem = path.name[: -len(extension)]
+    else:
+        stem = path.name
+    return path.with_name(f"{stem}-{h}{extension}")
 
 
 def get_loader_mapping() -> dict[str, BaseLoader]:
--- tests/unit/io/test_utils.py
+++ tests/unit/io/test_utils.py
@@ -1,10 +1,50 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+from unittest.mock import Mock, patch
+
 from coola.testing import torch_available
 
 from iden.io import JsonLoader, PickleLoader, TextLoader, TorchLoader, YamlLoader
-from iden.io.utils import get_loader_mapping
+from iden.io.utils import generate_unique_tmp_path, get_loader_mapping
 from iden.testing import yaml_available
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+##############################################
+#     Tests for generate_unique_tmp_path     #
+##############################################
+
+
+def test_generate_unique_tmp_path_no_suffix(tmp_path: Path) -> None:
+    with patch("iden.io.utils.uuid.uuid4", lambda: Mock(hex="a1b2c3")):
+        assert generate_unique_tmp_path(tmp_path.joinpath("data")) == tmp_path.joinpath(
+            "data-a1b2c3"
+        )
+
+
+def test_generate_unique_tmp_path_one_suffix(tmp_path: Path) -> None:
+    with patch("iden.io.utils.uuid.uuid4", lambda: Mock(hex="a1b2c3")):
+        assert generate_unique_tmp_path(tmp_path.joinpath("data.json")) == tmp_path.joinpath(
+            "data-a1b2c3.json"
+        )
+
+
+def test_generate_unique_tmp_path_two_suffixes(tmp_path: Path) -> None:
+    with patch("iden.io.utils.uuid.uuid4", lambda: Mock(hex="a1b2c3")):
+        assert generate_unique_tmp_path(tmp_path.joinpath("data.tar.gz")) == tmp_path.joinpath(
+            "data-a1b2c3.tar.gz"
+        )
+
+
+def test_generate_unique_tmp_path_dir(tmp_path: Path) -> None:
+    with patch("iden.io.utils.uuid.uuid4", lambda: Mock(hex="a1b2c3")):
+        assert generate_unique_tmp_path(tmp_path.joinpath("data/")) == tmp_path.joinpath(
+            "data-a1b2c3"
+        )
+
 
 ########################################
 #     Tests for get_loader_mapping     #
