773115ed5a4823d15b05ce3478914f3d2a8ca134
Thibaut Durand <durand.tibo@gmail.com>
2024-03-14 22:06:43 -0700

    Add `ShardTupleCreator`

--- src/iden/shard/creator/__init__.py
+++ src/iden/shard/creator/__init__.py
@@ -2,7 +2,8 @@ r"""Contain shard creator implementations."""
 
 from __future__ import annotations
 
-__all__ = ["BaseShardCreator", "JsonShardCreator"]
+__all__ = ["BaseShardCreator", "JsonShardCreator", "ShardTupleCreator"]
 
 from iden.shard.creator.base import BaseShardCreator
 from iden.shard.creator.json import JsonShardCreator
+from iden.shard.creator.tuple import ShardTupleCreator
