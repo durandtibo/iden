bb5ff24a781ff89303d0284d434eebd4178c73d6
Thibaut Durand <durand.tibo@gmail.com>
2024-12-27 12:52:04 -0800

    Add `is_cloudpickle_available` (#291)

--- src/iden/testing/__init__.py
+++ src/iden/testing/__init__.py
@@ -2,6 +2,10 @@ r"""Contain some utility functions for testing."""
 
 from __future__ import annotations
 
-__all__ = ["safetensors_available", "yaml_available"]
+__all__ = ["safetensors_available", "yaml_available", "cloudpickle_available"]
 
-from iden.testing.fixtures import safetensors_available, yaml_available
+from iden.testing.fixtures import (
+    cloudpickle_available,
+    safetensors_available,
+    yaml_available,
+)
