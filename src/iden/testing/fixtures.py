dc008ed00dfb4754c4033ffd73d7a5dfa8e1a658
Thibaut Durand <durand.tibo@gmail.com>
2025-12-28 08:12:45 -0800

    Add integration tests for imports

--- src/iden/testing/fixtures.py
+++ src/iden/testing/fixtures.py
@@ -2,7 +2,16 @@ r"""Define some PyTest fixtures."""
 
 from __future__ import annotations
 
-__all__ = ["cloudpickle_available", "joblib_available", "safetensors_available", "yaml_available"]
+__all__ = [
+    "cloudpickle_available",
+    "cloudpickle_not_available",
+    "joblib_available",
+    "joblib_not_available",
+    "safetensors_available",
+    "safetensors_not_available",
+    "yaml_available",
+    "yaml_not_available",
+]
 
 import pytest
 
@@ -13,11 +22,27 @@ from iden.utils.imports import (
     is_yaml_available,
 )
 
-cloudpickle_available = pytest.mark.skipif(
+cloudpickle_available: pytest.MarkDecorator = pytest.mark.skipif(
     not is_cloudpickle_available(), reason="Require cloudpickle"
 )
-joblib_available = pytest.mark.skipif(not is_joblib_available(), reason="Require joblib")
-safetensors_available = pytest.mark.skipif(
+cloudpickle_not_available: pytest.MarkDecorator = pytest.mark.skipif(
+    is_cloudpickle_available(), reason="Skip if cloudpickle is available"
+)
+joblib_available: pytest.MarkDecorator = pytest.mark.skipif(
+    not is_joblib_available(), reason="Require joblib"
+)
+joblib_not_available: pytest.MarkDecorator = pytest.mark.skipif(
+    is_joblib_available(), reason="Skip if joblib is available"
+)
+safetensors_available: pytest.MarkDecorator = pytest.mark.skipif(
     not is_safetensors_available(), reason="Require safetensors"
 )
-yaml_available = pytest.mark.skipif(not is_yaml_available(), reason="Require yaml")
+safetensors_not_available: pytest.MarkDecorator = pytest.mark.skipif(
+    is_safetensors_available(), reason="Skip if safetensors is available"
+)
+yaml_available: pytest.MarkDecorator = pytest.mark.skipif(
+    not is_yaml_available(), reason="Require yaml"
+)
+yaml_not_available: pytest.MarkDecorator = pytest.mark.skipif(
+    is_yaml_available(), reason="Skip if yaml is available"
+)
