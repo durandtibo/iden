97af9c799da4be1f34f431cca6ca33b2eeed0f93
Thibaut Durand <durand.tibo@gmail.com>
2025-12-29 09:18:00 -0800

    Remove incorrect `Self` type hints (#579)

--- src/iden/shard/comparators.py
+++ src/iden/shard/comparators.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 __all__ = ["ShardEqualityComparator"]
 
 import logging
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, TypeVar
 
 from coola.equality.comparators import BaseEqualityComparator
 from coola.equality.handlers import EqualNanHandler, SameObjectHandler, SameTypeHandler
@@ -15,16 +15,11 @@ from coola.equality.testers import EqualityTester
 from iden.shard.base import BaseShard
 
 if TYPE_CHECKING:
-    import sys
-
     from coola.equality import EqualityConfig
 
-    if sys.version_info >= (3, 11):
-        from typing import Self
-    else:
-        from typing_extensions import Self
-
 logger: logging.Logger = logging.getLogger(__name__)
+
+S = TypeVar("S", bound="ShardEqualityComparator")
 
 
 class ShardEqualityComparator(BaseEqualityComparator[BaseShard[Any]]):  # noqa: PLW1641
@@ -37,7 +32,7 @@ class ShardEqualityComparator(BaseEqualityComparator[BaseShard[Any]]):  # noqa: 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, self.__class__)
 
-    def clone(self) -> Self:
+    def clone(self) -> S:
         return self.__class__()
 
     def equal(self, actual: BaseShard[Any], expected: Any, config: EqualityConfig) -> bool:
