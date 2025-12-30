5368cdc83fc5e45963e304307c698c828357e87b
Thibaut Durand <durand.tibo@gmail.com>
2025-12-30 00:31:41 -0800

    Update `BaseShardLoader` (#616)

---
+++ tests/integration/shard/loader/test_coola.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import pytest
+from coola import objects_are_equal
+
+from iden.shard.loader import JsonShardLoader, PickleShardLoader
+
+#######################################
+#     Tests for objects_are_equal     #
+#######################################
+
+
+def test_objects_are_equal_true() -> None:
+    assert objects_are_equal(JsonShardLoader(), JsonShardLoader())
+
+
+def test_objects_are_equal_false() -> None:
+    assert not objects_are_equal(JsonShardLoader(), PickleShardLoader())
+
+
+@pytest.mark.parametrize("equal_nan", [True, False])
+def test_objects_are_equal_true_equal_nan(equal_nan: bool) -> None:
+    assert objects_are_equal(JsonShardLoader(), JsonShardLoader(), equal_nan=equal_nan)
