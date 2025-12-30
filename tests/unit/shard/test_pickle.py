e4cba8c3a71d4dc66ff89408c71c11ae81de8bde
Thibaut Durand <durand.tibo@gmail.com>
2025-12-30 07:57:49 -0800

    Fix `equal` for `PickleShard`

--- tests/unit/shard/test_pickle.py
+++ tests/unit/shard/test_pickle.py
@@ -31,6 +31,10 @@ def uri(tmp_path_factory: pytest.TempPathFactory, path: Path) -> str:
 #################################
 
 
+def test_pickle_shard_repr(uri: str, path: Path) -> None:
+    assert repr(PickleShard(uri=uri, path=path)).startswith("PickleShard(")
+
+
 def test_pickle_shard_str(uri: str, path: Path) -> None:
     assert str(PickleShard(uri=uri, path=path)).startswith("PickleShard(")
 
@@ -67,6 +71,12 @@ def test_pickle_shard_equal_false_different_path(uri: str, path: Path, tmp_path:
 
 def test_pickle_shard_equal_false_different_type(uri: str, path: Path) -> None:
     assert not PickleShard(uri=uri, path=path).equal(42)
+
+
+def test_pickle_shard_equal_false_different_type_child(uri: str, path: Path) -> None:
+    class Child(PickleShard): ...
+
+    assert not PickleShard(uri=uri, path=path).equal(Child(uri=uri, path=path))
 
 
 @pytest.mark.parametrize("equal_nan", [True, False])
